3f094bb685bbed4cc26484554435339578dbe1c8
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-08 09:58:57 -0500

    learning js functions and anonymous functions

---
+++ js/functions-practice.js
@@ -0,0 +1,12 @@
+function multiplication(x,y) {
+    return x * y;
+}
+function division(x,y) {
+    return x / y;
+}
+var multiply = function(x,y) {
+    return x * y;
+};
+var divide = function(x,y) {
+    return x / y;
+};
