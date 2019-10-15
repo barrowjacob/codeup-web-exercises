1626274d8296b176fd372cae25503d1ba536d1ec
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-15 15:49:42 -0500

    finished beginning of for-loop ex

---
+++ js/for_loops.js
@@ -0,0 +1,17 @@
+function showMultiplicationTable(x) {
+    for (var i = 0; i < 11; i++) {
+        console.log(x * i);
+    }
+}
+
+    for (var i = 0; i < 10; i++) {
+        var random = Math.floor(Math.random() * 20) + 100;
+        console.log(random);
+        if (random % 2 === 0) {
+            console.log("It's even!");
+        } else {
+            console.log("It's odd!");
+        }
+    }
+
+    // idk even wat for #4. nested for-loop? ugh. :-\ no se puede.
