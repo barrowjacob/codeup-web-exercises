3b85243d3ad37d641e5278c467d2b9df14e98478
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-30 09:20:23 -0500

    10/30 ex

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -1,6 +1,51 @@
 
 
     "use strict";
+    //10/30
+
+
+    var hamsters = [
+        {
+            name: "Hamtaro",
+            heightInMM: 86,
+            fur: ['orange', 'white'],
+            gender: "male",
+            dateOfBirth: "August 6"
+        } , {
+            name: "Bijou",
+            heightInMM: 75,
+            fur: ['white'],
+            gender: "female",
+            dateOfBirth: "July 10"
+        } , {
+            name: "Oxnard",
+            heightInMM: 100,
+            fur: ['grey', 'white'],
+            gender: "male",
+            dateOfBirth: "May 3"
+        } , {
+            name: "Boss",
+            heightInMM: 120,
+            fur: ['brown', 'white'],
+            gender: "male",
+            dateOfBirth: "Spetember 21"
+        } , {
+            name: "Snoozer",
+            heightInMM: 85,
+            fur: ['brown', 'white', "pink"],
+            gender: "male",
+            dateOfBirth: "January 14"
+        }
+    ];
+
+    function hamsterObjects(array) {
+       return array.sort(function(a,b) {
+            return a.heightInMM - b.heightInMM;
+        })
+
+    }
+console.log(hamsterObjects(hamsters));
+
 
     //10/29
 
