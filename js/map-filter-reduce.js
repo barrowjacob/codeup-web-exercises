ff398cbbc445ae62c0a2f3b2a786afba9003929b
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-12 15:57:26 -0600

    one remaining exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,56 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+
+var moreThanThree = users.filter((user) => {return user.languages.length >= 3});
+console.log(moreThanThree);
+
+var eachElementUserEmail = users.map((user) => {return user.email});
+console.log(eachElementUserEmail);
+
+var totalYearsExperience = users.reduce((total, current) => {return total + current.yearsOfExperience},0);
+console.log(totalYearsExperience);
+
+var longestEmail = users.reduce((a, b) => {
+    if (a.email.length > b.email.length) {
+    return a;
+        } else {
+        return b;
+    }
+});
+console.log(longestEmail);
