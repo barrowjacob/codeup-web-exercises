9760cec9e25e8908a6f5fa998b07ed0d6b4f32bc
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-28 09:31:23 -0600

    what's the opposite of a brain blast?

--- sandbox/Christmas-Sandbox.js
+++ sandbox/Christmas-Sandbox.js
@@ -1,10 +1,13 @@
-(function () {
 
-    document.getElementsByTagName("th").addEventListener("click", openWindow);
+    let newColor = document.getElementsByTagName("body")[0];
+    newColor.style['background-color'] = "#f233f3";
+    // let newColor = document.getElementsByTagName("body")[0];
+    // newColor.style['background-color'] = "#f233f3";
+    // function newFunction() {
+    //     document.getElementsByTagName("body").style.backgroundColor = "#abc248";
+    // };
+// document.getElementById("li-1").on('click', function() {
+//     document.getElementById("li-1").innerHTML = "wat";
+// })
 
-    function openWindow() {
-        window.open("https://www.google.com/");
-    }
-    // $('#firstAlbum').on('click', openWindow());
 
-});
