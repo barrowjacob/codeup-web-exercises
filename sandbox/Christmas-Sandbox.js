dc5c385b8f0a2f0b90c73289a1291526b5c38b00
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-28 13:58:09 -0600

    fixed click functionality, added append button

--- sandbox/Christmas-Sandbox.js
+++ sandbox/Christmas-Sandbox.js
@@ -1,13 +1,17 @@
+$("#li-1").click(function(){
+    window.open("https://www.youtube.com/watch?v=w9QLn7gM-hY");
+});
+$("#li-2").click(function(){
+    window.open("https://www.youtube.com/watch?v=Ae7CtryWCkc&list=OLAK5uy_l6olFPG-UrfC2EJFciCWv5cbYBoesK_0I");
+});
+$("#li-3").click(function(){
+    window.open("https://www.youtube.com/watch?v=aRmcbo5RAks&list=PLPP6w1_F70wkaLp2hynlPPzEkADGz5fza&index=4");
+});
+var num = 4;
+$("#inputButton").click(function(){
+    $("#ul-1").append("<li class='list-item' id='li-$(\'num\')'" + "><i class='fab fa-youtube'></i>" + "\"" + $("#inputText").val() + "\"</li>");
+    num++;
+});
 
-    let newColor = document.getElementsByTagName("body")[0];
-    newColor.style['background-color'] = "#f233f3";
-    // let newColor = document.getElementsByTagName("body")[0];
-    // newColor.style['background-color'] = "#f233f3";
-    // function newFunction() {
-    //     document.getElementsByTagName("body").style.backgroundColor = "#abc248";
-    // };
-// document.getElementById("li-1").on('click', function() {
-//     document.getElementById("li-1").innerHTML = "wat";
-// })
 
 
